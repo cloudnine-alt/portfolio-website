3d4a9cc0fbfc67a0b7484e319b614ec3f2af2985
Your Name <your@email.com>
2025-02-23 05:37:00 -0500

    Initial commit: Portfolio website setup

---
+++ src/test/java/com/myportfolioRC/portfolio/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolioRC.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
